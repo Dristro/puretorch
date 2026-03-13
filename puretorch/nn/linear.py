5b820274d7518bf738d1b0e44afbd79c04d7e3c4
Dhruv N <tankig912@gmail.com>
2026-03-14 02:37:19 +0530

    feat (linear): parameters initialized using name

--- puretorch/nn/linear.py
+++ puretorch/nn/linear.py
@@ -1,16 +1,10 @@
 import numpy as np
-from puretorch import Tensor, nn
+from puretorch import nn, Tensor
+
 
 class Linear(nn.Module):
-    def __init__(self,
-                 in_features: int,
-                 out_features: int,
-                 bias: bool = True):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True):
         """
-        NOTE: This class is likely to not work the updated version of Tensor.
-              This method works for the version of `PureTorch.Tensor` that doesn't use NumPy.
-
-        Creates a layers of `Perceptrons` (found at PureTorch.nn.Perceptron)
         Args:
             in_features   - number of inputs the layer expects
             out_features  - number of perceptrons in layer (number of outputs of the layer)
@@ -27,22 +21,30 @@ class Linear(nn.Module):
         weights_norm = np.linalg.norm(weights_data)
         epsilon = 1e-8
         if weights_norm != 0:
-            weights_data /= (weights_norm + epsilon)
-        #self.weights = Tensor(weights_data, requires_grad=True)
-        self.weights = nn.Parameter(weights_data, requires_grad=True)
+            weights_data /= weights_norm + epsilon
+
+        self.weights = nn.Parameter(
+            weights_data,
+            name="w_linear",
+            requires_grad=True,
+        )
 
         # Bias (normalized, -1 to 1)
         if bias:
             bias_data = np.random.randn(out_features)
             bias_norm = np.linalg.norm(bias_data)
             if bias_norm != 0:
-                bias_data /= (bias_norm + epsilon)
-            #self.bias = Tensor(bias_data, requires_grad=True)
-            self.bias = nn.Parameter(bias_data, requires_grad=True)
+                bias_data /= bias_norm + epsilon
+
+            self.bias = nn.Parameter(
+                bias_data,
+                name="b_linear",
+                requires_grad=True,
+            )
         else:
             self.bias = None
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         """
         Performs a forward pass using the layers weights and bias
 
@@ -53,19 +55,10 @@ class Linear(nn.Module):
             list of outputs of the layer
         """
         # assert isinstance(x, Tensor), "Input x must be a Tensor."
-        assert x.data.shape[-1] == self.in_features, (
-            f"Expected input features {self.in_features}, "
-            f"but got {x.data.shape[-1]}."
-        )
+        assert x.data.shape[-1] == self.in_features, f"Expected input features {
+            self.in_features
+        }, but got {x.data.shape[-1]}."
         out = x @ self.weights.T
         if self.bias is not None:
             out = out + self.bias
         return out
-
-    #def parameters(self):
-    #    yield self.weights
-    #    if self.bias is not None:
-    #        yield self.bias
-
-    #def __call__(self, x):
-    #    return self.forward(x)
