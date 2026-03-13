55669198d389153749b280f84656c7966bfdc05d
Dhruv N <tankig912@gmail.com>
2026-03-14 02:38:06 +0530

    feat (optimizer): robust param_group initialization

--- puretorch/optim/optimizer.py
+++ puretorch/optim/optimizer.py
@@ -1,35 +1,98 @@
-import puretorch
-from typing import DefaultDict, List, Dict, Any, cast
+"""
+Base class for all optimizer classes, be it custom or defaults
+"""
+
 from collections import defaultdict
 from abc import abstractmethod
-# Base class for all optimizer classes, be it custom or defaults
+from typing import Iterable, Any
+
+from puretorch import nn
+
 
 class Optimizer:
     """
     Base class for all optimizers
+
+    Once initialized, following attrs are availible:
+    - param_groups (list[dict[str, Any]])
+
     Args:
         params: iterable of PureTorch.Tensor's. Parameters to optimized
     """
-    def __init__(self, params):
-        if not isinstance(params, (tuple, list)) and not hasattr(params, "__iter__"):
-            raise TypeError("Parameters must be an iterable with Tensors or dicts, got: "
-                            + params.__class__.__name__)
 
-        self.state: DefaultDict[puretorch.Tensor, Any] = defaultdict(dict)
-        self.param_groups: List[Dict[str, Any]] = []
+    def __init__(self, params: Iterable[nn.Parameter], defaults: dict):
+        """
+        Initializes param_groups as a list of dicts. Where each dict
+        contains a single kv pair with key `params` associated with a
+        list of parameters.
+
+        Args:
+            params (Iterable): an iterable of nn.Parameter. Parameters
+            to be optimized
+            defaults (dict): dict containing default optimization opts
+        """
+        self.defaults = defaults
+
+        self.state = defaultdict(dict)
+        self.param_groups: list[dict[str, Any]] = []
 
         param_groups = list(params)
-        if not param_groups or len(param_groups) == 0:  # Check for no parameters
-            raise ValueError("No parameters to optimize.")
+        if len(param_groups) == 0:
+            raise ValueError("Got empty parameter list.")
         if not isinstance(param_groups[0], dict):
             param_groups = [{"params": param_groups}]
-        self.param_groups = param_groups # List of Dicts, where key = "params"
+
+        for param_group in param_groups:
+            self.add_param_group(param_group)
+
+    def add_param_group(self, param_group: dict[str, Any]):
+        """
+        Add a param group to Optimizer.
+
+        Args:
+            param_group (dict)
+
+        """
+        assert isinstance(param_group, dict), f"param group must be a dict, got {
+            type(param_group)
+        }"
+
+        params = param_group["params"]
+        if isinstance(params, nn.Parameter):
+            param_group["params"] = [params]
+        elif isinstance(params, set):
+            raise TypeError(
+                "Optimizer params are expected to be ordered. Got a set, whose order changes bw runs..."
+            )
+        else:
+            param_group["params"] = list(params)
+
+        for param in param_group["params"]:
+            if not isinstance(param, nn.Parameter):
+                raise TypeError(f"Expected nn.Parameter, got {type(param)}.")
+            if not param.is_leaf:
+                raise ValueError("Optimizing a leaf Tensor is not allowed.")
+
+            for name, default in self.defaults.items():
+                param_group.setdefault(name, default)
+
+            # Ensure unique params per param-group
+            param_set = set()
+            for group in self.param_groups:
+                param_set.update(set(group["params"]))
+            if not param_set.isdisjoint(set(param_group["params"])):
+                raise ValueError("Some params appear in more than one param group")
+
+            self.param_groups.append(param_group)
 
     def zero_grad(self, set_to_none: bool = False):
         """
-        Sets the gradients of all the parameters given to the optimizer
+        Sets the gradients of all the parameters given to the optimizer.
+        Please note that all params in param_groups are set to zero-grad.
+
         Args:
-            set_to_none: sets the grads to None if True, sets the grads to zero otherwise.
+            set_to_none (bool, default=False): sets the grads to None if True,
+            zero otherwise.
         """
         for group in self.param_groups:
             for param in group["params"]:
@@ -40,8 +103,10 @@ class Optimizer:
                         try:
                             param.zero_grad()
                         except AttributeError:
-                            raise TypeError("Parameter does not support 'zero_grad'; ensure all parameters are "
-                                            "PureTorch.Tensors.")
+                            raise TypeError(
+                                "Parameter does not support 'zero_grad'; ensure all parameters are "
+                                "puretorch.Tensors."
+                            )
 
     # The step method must be defined for each optimizer
     @abstractmethod
