f408ef775fe9241f801c4b107f94086f8b43a97d
Dhruv N <tankig912@gmail.com>
2026-03-14 02:36:27 +0530

    feat (parameter.py): keeps track of name

--- puretorch/nn/parameter.py
+++ puretorch/nn/parameter.py
@@ -1,10 +1,47 @@
+import numpy as np
+
 from puretorch import Tensor
+from autograd import Variable
+from typing import Literal, get_args
+
+AllowedName = Literal[
+    "w_linear",
+    "b_linear",
+]
+_ALLOWED_NAMES = get_args(AllowedName)
+
 
 class Parameter(Tensor):
-    """Simple alias to mark a Tensor as trainable parameter."""
-    def __init__(self, data, requires_grad: bool = True):
+    """
+    Alias to mark a Tensor as trainable parameter.
+
+    Additional to Tensor, Parameter also stores information like
+    `name`. This will be particularly helpful for Module specific
+    operations.
+    """
+
+    def __init__(
+        self,
+        data: int | float | list | tuple | np.ndarray | Tensor | Variable,
+        name: AllowedName,
+        requires_grad: bool = True,
+    ):
+        """
+        Args:
+            data (see Tensor docs for dtype): data for the tensor
+            name (str): name given to parameter
+            requires_grad (bool, default=True): requires grad
+        """
         super().__init__(
             data,
             requires_grad=requires_grad,
-            is_leaf=True
+            is_leaf=True,
+            device="cpu",
         )
+
+        if name not in _ALLOWED_NAMES:
+            raise ValueError(
+                f"Unsupported `name`, expected one of "
+                f"({', '.join(_ALLOWED_NAMES)}), got '{name}'."
+            )
+        self.name = name
