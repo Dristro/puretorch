97c19f0e1311771f69f6c48d597f2c9f85d8f371
Dhruv N <tankig912@gmail.com>
2026-03-20 01:31:27 +0530

    refactor (Function): make forward/backward static methods for correct dispatch

--- autograd/function.py
+++ autograd/function.py
@@ -1,9 +1,17 @@
+import numpy as np
+
 from .context import Context
+
 
 class Function:
     """Base class for autograd functions."""
+
+    _parents = []
+
+    @staticmethod
     def forward(ctx: Context, *args, **kwargs):
         raise NotImplementedError
 
-    def barkward(ctx: Context, grad_output):
+    @staticmethod
+    def barkward(ctx: Context, grad_output: np.ndarray):
         raise NotImplementedError
