5062a62e277ef5125d5671482ddb960483996d1f
Dhruv N <tankig912@gmail.com>
2026-03-20 01:40:26 +0530

    refactor (summary): disable summary

--- puretorchinfo/summary.py
+++ puretorchinfo/summary.py
@@ -1,15 +1,14 @@
-import puretorch
 from puretorch import nn
 
 def summary(model: nn.Module):
     """
     Prints the model's layer name along with the total parameter count.
     """
-    raise NotImplementedError(f"Will add this in a future version...")
+    raise NotImplementedError("Will add this in a future version...")
     # ignore this code, needs to be updated...
     print(f"Model: {[x]}\n")
-    print(f"Layer name\t| Num params")
-    print(f"---------------\t| ---------")
+    print("Layer name\t| Num params")
+    print("---------------\t| ---------")
     sum = 0
     for layer in layers:
         print(f"{layer.__class__.__name__}\t\t| {len(layer.parameters())}")
